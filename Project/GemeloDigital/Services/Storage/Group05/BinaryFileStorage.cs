97d4649199e494df64e06f05c65d94681286b854
AaronMartinez91 <aaronalejandro2@gmail.com>
2025-11-21 13:28:38 +0100

    -Dummy. + Binary
    Añadimos clase BinaryFileStorage y cambiamos el acceso del core de dummy a esta.

---
+++ Project/GemeloDigital/Services/Storage/Group05/BinaryFileStorage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemeloDigital
+{
+    internal class BinaryFileStorage : Storage
+    {
+        List<string> list = new List<string>();
+
+        internal override void Initialize()
+        {
+            //Console.WriteLine("BinaryFileStorage: Initializing");
+
+            list = new List<string>();
+
+            list.Add("TestScene1");
+            list.Add("TestScene2");
+            list.Add("TestScene3");
+        }
+
+        internal override void Finish()
+        {
+            //Console.WriteLine("BinaryFileStorage: Finish");
+        }
+
+        internal override void LoadScene(string storageId)
+        {
+            //Console.WriteLine("BinaryFileStorage: Load simulation" + storageId);
+        }
+
+        internal override void SaveScene(string storageId)
+        {
+            //Console.WriteLine("BinaryFileStorage: Save simulation " + storageId);
+            list.Add(storageId);
+        }
+
+        internal override void DeleteScene(string storageId)
+        {
+            //Console.WriteLine("Deleting simulation " + storageId);
+            list.Remove(storageId);
+        }
+
+        internal override List<string> ListScenes()
+        {
+            return list;list = new List<string>();
+        }
+
+
+    }
+}
