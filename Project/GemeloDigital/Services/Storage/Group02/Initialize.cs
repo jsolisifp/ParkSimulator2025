204f6cc83140ecd58c8e3a59d6f5f446618395d6
Elias032005 <delgadooelias@gmail.com>
2025-11-27 12:39:29 +0100

    primero partials

---
+++ Project/GemeloDigital/Services/Storage/Group02/Initialize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemeloDigital
+{
+    internal partial class Storage02: Storage
+    {
+        internal override void Initialize()
+        {
+            Console.WriteLine("DummyStorage: Initializing");
+            lista_storages = new List<string>();
+
+
+
+
+
+
+            //lista_storages.Add("Guardado1.dat");
+            //lista_storages.Add("Guardado2.dat");
+            //lista_storages.Add("Guardado3.dat");
+        }
+
+    }
+}
---
+++ Project/GemeloDigital/Services/Storage/Group02/Storage02.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemeloDigital
+{
+    internal partial class Storage02 : Storage
+    {
+        //variables generales de la clase storage
+        //--> las funciones estan en los archivos parciales
+        List<string> lista_storages = new List<string>();
+
+
+    }
+}
