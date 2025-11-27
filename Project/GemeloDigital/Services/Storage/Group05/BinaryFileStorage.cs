1a62f28b4a47781e9f30e4ec7c02347701ccc68b
Lord-Pat <153514448+Lord-Pat@users.noreply.github.com>
2025-11-27 14:14:46 +0100

    V0.1 Points
    Primer prototipo (sin probar) de guardado de puntos en formato bit.

--- Project/GemeloDigital/Services/Storage/Group05/BinaryFileStorage.cs
+++ Project/GemeloDigital/Services/Storage/Group05/BinaryFileStorage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,7 +35,51 @@ namespace GemeloDigital
         internal override void SaveScene(string storageId)
         {
             //Console.WriteLine("BinaryFileStorage: Save simulation " + storageId);
-            list.Add(storageId);
+
+            // Guardar puntos
+            byte[] bytesNum = new byte[sizeof(int)];
+            byte[] bytesStr;
+
+            string scenePointsName = storageId + "points.dat";
+            FileStream streamPoints = new FileStream(scenePointsName, FileMode.Create, FileAccess.Write);
+
+            // Sacamos la lista de puntos (ojo! devuelve lista de simulated objetcs con type punto que hay convertir a puntos)
+
+            List<SimulatedObject> objectsPoint = SimulatorCore.FindObjectsOfType(SimulatedObjectType.Point);
+            List<Point> points = new List<Point>(); 
+            Point point = new Point();
+            
+            foreach (SimulatedObject o in objectsPoint)
+            {
+               point = SimulatorCore.AsPoint(o);
+               points.Add(point);
+            }
+
+            // Forma de guardar los puntos
+            // Espacio en bytes del Id
+            // Id (str)
+            // Tamaño de bytes del nombre en UTF8
+            // Nombre en UTF8
+            // coordenadas en float (orden X,Y,Z)
+            foreach(Point p in points)
+            {
+                bytesStr = System.Text.Encoding.UTF8.GetBytes(p.Id);
+                streamPoints.Write(BitConverter.GetBytes(bytesStr.Length));
+                streamPoints.Write(bytesStr);
+
+                bytesStr = System.Text.Encoding.UTF8.GetBytes(p.Name);
+                streamPoints.Write(BitConverter.GetBytes(bytesStr.Length));
+                streamPoints.Write(bytesStr);
+
+                bytesNum = BitConverter.GetBytes(p.Position.X);
+
+                bytesNum = BitConverter.GetBytes(p.Position.Y);
+
+                bytesNum = BitConverter.GetBytes(p.Position.Z);
+            }
+
+            streamPoints.Close();
+            
         }
 
         internal override void DeleteScene(string storageId)
