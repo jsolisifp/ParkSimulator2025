569d460f3d14b2a3a6862b43c4e9613b7e728d11
miquel06 <miquelfontana08@gmail.com>
2025-12-11 13:58:40 +0100

    CREAR LISTAS , GUARDAR POINTS , GUARDAR FACILITIES

--- Project/GemeloDigital/Services/Storage/Group06/cargarguardar.cs
+++ Project/GemeloDigital/Services/Storage/Group06/cargarguardar.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace GemeloDigital
 {
@@ -10,55 +10,70 @@ namespace GemeloDigital
     {
         List<string> list = new List<string>();
 
-        internal override void Initialize()
-        {
-            //Console.WriteLine("DummyStorage: Initializing");
-
-            list = new List<string>();
-
-            list.Add("TestScene1");
-            list.Add("TestScene2");
-            list.Add("TestScene3");
-        }
-
-        internal override void Finish()
-        {
-            Console.WriteLine("DummyStorage: Finish");
-        }
-        
-        internal override void LoadScene(string storageId)
-        {
-            Console.WriteLine("DummyStorage: Load simulation" + storageId);
-        }
-
         internal override void SaveScene(string storageId)
         {
             Console.WriteLine("Grupo 6: Save simulation " + storageId);
+            string filename = GetCorrectFilename(storageId);
+            Console.WriteLine("Guardando archivo en: " + System.IO.Path.GetFullPath(filename));
 
-            //Console.ReadLine();
+            FileStream fichero = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            byte[] bytes;
 
+            List<SimulatedObject> allObjects = SimulatorCore.GetObjects();
+            List<Point> points = new List<Point>();
+            List<Facility> facilities = new List<Facility>();
+            List<Path> paths = new List<Path>();
+            List<Person> persons = new List<Person>();
 
-            list.Add(storageId);
+            foreach (var obj in allObjects)
+            {
+                if (obj.Type == SimulatedObjectType.Point) points.Add(SimulatorCore.AsPoint(obj));
+                else if (obj.Type == SimulatedObjectType.Facility) facilities.Add(SimulatorCore.AsFacility(obj));
+                else if (obj.Type == SimulatedObjectType.Path) paths.Add(SimulatorCore.AsPath(obj));
+                else if (obj.Type == SimulatedObjectType.Person) persons.Add(SimulatorCore.AsPerson(obj));
+            }
+
+            // Guardar Points
+            bytes = BitConverter.GetBytes(points.Count); fichero.Write(bytes, 0, bytes.Length);
+            foreach (var po in points)
+            {
+                byte[] idBytes = Encoding.UTF8.GetBytes(po.Id);
+                fichero.Write(BitConverter.GetBytes(idBytes.Length), 0, sizeof(int));
+                fichero.Write(idBytes, 0, idBytes.Length);
+                byte[] nameBytes = Encoding.UTF8.GetBytes(po.Name);
+                fichero.Write(BitConverter.GetBytes(nameBytes.Length), 0, sizeof(int));
+                fichero.Write(nameBytes, 0, nameBytes.Length);
+                fichero.Write(BitConverter.GetBytes(po.Position.X), 0, sizeof(float));
+                fichero.Write(BitConverter.GetBytes(po.Position.Y), 0, sizeof(float));
+                fichero.Write(BitConverter.GetBytes(po.Position.Z), 0, sizeof(float));
+            }
+            // Guardar Facilities
+            bytes = BitConverter.GetBytes(facilities.Count);
+            fichero.Write(bytes, 0, bytes.Length);
+            foreach (var f in facilities)
+            {
+                byte[] idBytes = Encoding.UTF8.GetBytes(f.Id);
+                fichero.Write(BitConverter.GetBytes(idBytes.Length), 0, sizeof(int));
+                fichero.Write(idBytes, 0, idBytes.Length);
+                byte[] nameBytes = Encoding.UTF8.GetBytes(f.Name);
+                fichero.Write(BitConverter.GetBytes(nameBytes.Length), 0, sizeof(int));
+                fichero.Write(nameBytes, 0, nameBytes.Length);
+                fichero.Write(BitConverter.GetBytes(f.PowerConsumed), 0, sizeof(float));
+                fichero.Write(BitConverter.GetBytes(f.Entrances.Count), 0, sizeof(int));
+                foreach (var ent in f.Entrances)
+                {
+                    byte[] pid = Encoding.UTF8.GetBytes(ent.Id);
+                    fichero.Write(BitConverter.GetBytes(pid.Length), 0, sizeof(int));
+                    fichero.Write(pid, 0, pid.Length);
+                }
+                fichero.Write(BitConverter.GetBytes(f.Exits.Count), 0, sizeof(int));
+                foreach (var ext in f.Exits)
+                {
+                    byte[] pid = Encoding.UTF8.GetBytes(ext.Id);
+                    fichero.Write(BitConverter.GetBytes(pid.Length), 0, sizeof(int));
+                    fichero.Write(pid, 0, pid.Length);
+                }
+            }
         }
-
-        internal override void DeleteScene(string storageId)
-        {
-            Console.WriteLine("Deleting simulation " + storageId);
-            list.Remove(storageId);
-        }
-        void SavePerson(Person p, FileStream stream)
-        {
-
-            stream  = new FileStream ("guardado" , FileMode.Open ,FileAccess.Read);
-        }
-        
-        
-
-        internal override List<string> ListScenes()
-        {
-            return list;list = new List<string>();
-        }
-
-
     }
 }
